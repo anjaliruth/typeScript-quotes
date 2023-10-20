5bb0f7bcb97b7b0b73d9e3aa1b0cd5238b191a73
Anjali Ruth Muthusamy <124257618+anjaliruth@users.noreply.github.com>
2023-10-20 22:12:42 +0100

    can display cocktail on page based on input search or category

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,66 @@ import QuoteDisplay from "./components/QuoteDisplay";
 import { useState, createContext } from "react";
 import RecipeDisplay from "./components/RecipeDisplay";
 
+export interface Recipe {
+  idDrink: string;
+  strDrink: string;
+  strDrinkAlternate?: string;
+  strTags?: string;
+  strVideo?: string;
+  strCategory?: string;
+  strIBA?: string;
+  strAlcoholic?: string;
+  strGlass?: string;
+  strInstructions?: string;
+  strInstructionsES?: string;
+  strInstructionsDE?: string;
+  strInstructionsFR?: string;
+  strInstructionsIT?: string;
+  strInstructionsZH_HANS?: string;
+  strInstructionsZH_HANT?: string;
+  strDrinkThumb?: string;
+  strIngredient1?: string;
+  strIngredient2?: string;
+  strIngredient3?: string;
+  strIngredient4?: string;
+  strIngredient5?: string;
+  strIngredient6?: string;
+  strIngredient7?: string;
+  strIngredient8?: string;
+  strIngredient9?: string;
+  strIngredient10?: string;
+  strIngredient11?: string;
+  strIngredient12?: string;
+  strIngredient13?: string;
+  strIngredient14?: string;
+  strIngredient15?: string;
+  strMeasure1?: string;
+  strMeasure2?: string;
+  strMeasure3?: string;
+  strMeasure4?: string;
+  strMeasure5?: string;
+  strMeasure6?: string;
+  strMeasure7?: string;
+  strMeasure8?: string;
+  strMeasure9?: string;
+  strMeasure10?: string;
+  strMeasure11?: string;
+  strMeasure12?: string;
+  strMeasure13?: string;
+  strMeasure14?: string;
+  strMeasure15?: string;
+  strImageSource?: string;
+  strImageAttribution?: string;
+  strCreativeCommonsConfirmed?: string;
+  dateModified?: string;
+}
+
 function App() {
   const [search, setSearch] = useState<string>("");
   const [spiritCategory, setSpiritCategory] = useState<string>("");
-  const [recipe, setRecipe] = useState<any>([])
+  const [recipe, setRecipe] = useState<Recipe[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
 
   console.log(search);
   console.log(spiritCategory);
@@ -30,7 +86,7 @@ function App() {
         <option value="Vodka">Vodka</option>
         <option value="Gin">Gin</option>
       </select>
-      <RecipeDisplay search={search} spiritCategory={spiritCategory} setRecipe={setRecipe}/>
+      <RecipeDisplay search={search} spiritCategory={spiritCategory} setRecipe={setRecipe} recipe={recipe} isLoading={isLoading} setIsLoading={setIsLoading}/>
     </div>
   );
 }
--- src/components/RecipeDisplay.tsx
+++ src/components/RecipeDisplay.tsx
@@ -1,38 +1,63 @@
 import React, { useEffect } from "react";
+import { Recipe } from "../App";
+
 interface RecipeDisplayProps {
   search: string;
   spiritCategory: string;
   setRecipe: React.Dispatch<React.SetStateAction<any>>;
+  recipe: Recipe[];
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isLoading: boolean
 }
 
 export default function RecipeDisplay({
   search,
   spiritCategory,
   setRecipe,
+  recipe, setIsLoading, isLoading
 }: RecipeDisplayProps) {
-  async function fetchByCategory(): Promise<void> {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spiritCategory}`
-    );
-
-    const data: any = await response.json();
-
-    setRecipe(data);
-  }
-
-  async function fetchBySearch(): Promise<void> {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${search}`
-    );
-
-    const data: any = await response.json();
-
-    setRecipe(data);
+  async function fetchRecipes(apiURL: string): Promise<void> {
+    try {
+      const response = await fetch(apiURL);
+      const data: any = await response.json();
+      setRecipe(data.drinks);
+    } catch (e) {
+      console.error("Error fetching results:", e);
+    } finally{
+      setIsLoading(false)
+    }
   }
 
   useEffect(() => {
-    fetchByCategory();
-  }, [spiritCategory]);
+    let apiURL = "";
+    setIsLoading(true)
+    if (spiritCategory) {
+      apiURL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spiritCategory}`;
+    } else if (search) {
+      apiURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`;
+    }
 
-  return <div>RecipeDisplay</div>;
+    if (apiURL) {
+      fetchRecipes(apiURL);
+    }
+
+    else{
+      setRecipe([])
+      setIsLoading(false)
+    }
+  }, [spiritCategory, search]);
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {!isLoading && recipe.map((cocktail, i) => {
+        return (
+
+          <div className="" key={i}>
+            <h2>{cocktail.strDrink}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
 }

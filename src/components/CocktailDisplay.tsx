9ff1f1069cda54362ff1d5b1ab142d73a5808b1f
Anjali Ruth Muthusamy <124257618+anjaliruth@users.noreply.github.com>
2023-10-30 10:58:13 +0000

    added user feedback for empty search

--- src/components/CocktailDisplay.tsx
+++ src/components/CocktailDisplay.tsx
@@ -6,12 +6,12 @@ interface CocktailDisplayProps {
   search: string;
   spiritCategory: string;
   setResult: React.Dispatch<React.SetStateAction<any>>;
-  result: Recipe[];
+  result: Recipe[] | null;
   setRecipe: React.Dispatch<React.SetStateAction<any>>;
   recipe: Recipe;
   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
   isLoading: boolean;
-  fetchRecipesByCategory:() => Promise<void>
+  fetchRecipesByCategory: () => Promise<void>;
 }
 
 export default function CocktailDisplay({
@@ -23,14 +23,14 @@ export default function CocktailDisplay({
   isLoading,
   recipe,
   setRecipe,
-  fetchRecipesByCategory, 
+  fetchRecipesByCategory,
 }: CocktailDisplayProps) {
-  const {spirit, drink} = useParams()
-  
+  const { spirit, drink } = useParams();
 
-  console.log("SPIRIT", spirit)
+  console.log("SPIRIT", spirit);
+
   useEffect(() => {
-fetchRecipesByCategory()
+    fetchRecipesByCategory();
   }, [spiritCategory]);
 
   async function fetchRecipeByID(idDrink: string): Promise<void> {
@@ -40,30 +40,30 @@ fetchRecipesByCategory()
     const data = await response.json();
     setRecipe(data.drinks[0]);
   }
-  
 
   return (
     <div className=" bg-pink-200">
+      <h1 className="text-3xl font-bold text-center p-5">{spirit} Cocktails</h1>
 
-
-
-
-<h1 className="text-3xl font-bold text-center p-5">{spirit} Cocktails</h1>
-
-      {isLoading && <p>Loading...</p>}
-      <div className="grid grid-cols-3 m-auto bg-purple-400 gap-4 place-items-center w-4/5">
-      {!isLoading && 
-        result.map((cocktail, i) => {
-          return (
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : result && result.length > 0 ? (
+        <div className="grid grid-cols-3 m-auto bg-purple-400 gap-4 place-items-center w-4/5">
+          {result.map((cocktail, i) => (
             <div key={cocktail.idDrink} className="flex flex-col items-center p-2 bg-blue-200 gap-4 rounded-[10px]">
               <h2 className="text-xl font-bold">{cocktail.strDrink}</h2>
-              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} className="w-3/5 max-lg rounded-[15px] "/>
-             <Link to={`/${cocktail.strDrink}/recipe`} > <button onClick={()=>fetchRecipeByID(cocktail.idDrink)} className="mb-3 p-2 rounded bg-green-200">🍸 View Recipe</button>
-             </Link>
+              <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} className="w-3/5 max-lg rounded-[15px] " />
+              <Link to={`/${cocktail.strDrink}/recipe`}>
+                <button onClick={() => fetchRecipeByID(cocktail.idDrink)} className="mb-3 p-2 rounded bg-green-200">
+                  🍸 View Recipe
+                </button>
+              </Link>
             </div>
-          );
-        })}
+          ))}
         </div>
+      ) : (
+        <p className=" text-2xl text-center font-medium">☹️ Sorry! No such cocktail found.</p>
+      )}
     </div>
   );
 }
